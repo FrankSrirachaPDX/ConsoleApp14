120fc8b4cb96f0738eaef3f7145768ea9026b8df
Ryan <werner.ryan18@gmail.com>
2018-10-10 13:04:26 -0700

    Add project files.

---
+++ ConsoleApp14/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class Class1
+    {
+
+        public static int Math1(int x)
+        {
+            x = x + 20;
+
+            return x;
+        }
+
+        public static int Math2(double y)
+        {
+            int num;
+            y = y / 2;
+            num = Convert.ToInt32(y);
+
+            return num;
+        }
+
+        public static int Math3(string z)
+        {
+            int num;
+            num = Convert.ToInt32(z);
+            num = num + 2;
+            return num;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 holder = new Class1();
+            string temp;
+            int numInt;
+            double numDub;
+
+            Console.WriteLine("Enter an integer");
+            temp = Console.ReadLine();
+            numInt = Convert.ToInt32(temp);
+            numInt = Class1.Math1(numInt);
+            Console.WriteLine(numInt);
+
+            Console.WriteLine("Enter a decimal");
+            temp = Console.ReadLine();
+            numDub = Convert.ToDouble(temp);
+            numInt = Class1.Math2(numDub);
+            Console.WriteLine(numInt);
+
+            Console.WriteLine("Enter a string!");
+            temp = Console.ReadLine();
+            numInt = Class1.Math3(temp);
+            Console.WriteLine(numInt);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
